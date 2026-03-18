359d0661d437e7f9e342c4d4aaa39530a95b4657
mioty <hei.mioty.2@gmail.com>
2026-03-18 17:06:20 +0300

    TD1 initial

---
+++ src/main/java/com/example/rest_serviceee/RestServiceeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_serviceee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rest_serviceee/RestServiceeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_serviceee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
